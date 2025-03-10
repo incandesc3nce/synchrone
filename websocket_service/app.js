08ee8ed4edc6f1193d23f3c46230a30c1a0c15c9
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-03-10 20:27:11 +0700

    feat(websocket_service): replace ws with socket.io library

--- websocket_service/app.js
+++ websocket_service/app.js
@@ -1,35 +1,36 @@
-const WebSocketServer = require('ws').Server;
-
-const wss = new WebSocketServer({ port: 8080 });
+import { Server } from "socket.io";
+import 'dotenv/config';
 
 const logAction = (action, size) => {
   console.log(action);
   console.log(`Clients amount: ${size}`);
 }
 
-let msg = '// Welcome to the synchrone WebSocket server';
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    console.log('received: %s', data);
-    const received = JSON.parse(data);
-    const message = received.message.toString();
-    msg = message;
-    const type = received.type;
-    
-    // Broadcast to all clients
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ message, type }));
-      }
-    });
-  });
-
-  ws.on('close', function close() {
-    logAction('Client disconnected', wss.clients.size);
-  });
-
-  ws.send(JSON.stringify({ message: msg, type: 'code' }));
-  logAction('Client connected', wss.clients.size);
+const io = new Server(8080, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+  }
 });
 
-console.log('WebSocket server started on ws://localhost:8080');
+let msg = '// Welcome to the synchrone WebSocket server';
+
+io.on('connection', (socket) => {
+  socket.on('message', (data) => {
+    console.log('📩 received: %s', data);
+    const message = data.message;
+    msg = message
+    const type = data.type;
+    
+    // Broadcast to all clients
+    io.emit('message', { message, type });
+  });
+
+  socket.on('disconnect', () => {
+    logAction('🔥 Client disconnected', io.engine.clientsCount);
+  });
+
+  socket.emit('message', { message: msg, type: 'code' });
+  logAction('⚡ Client connected', io.engine.clientsCount);
+})
+
+console.log('🚀 Socket.io server is running on 127.0.0.1:8080');
