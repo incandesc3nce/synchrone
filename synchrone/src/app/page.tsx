22abcf327567c79511b79baaf46fc2a6b150f0b1
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-04-20 16:21:34 +0700

    feat: add footer

--- synchrone/src/app/page.tsx
+++ synchrone/src/app/page.tsx
@@ -5,12 +5,15 @@ export default function HomePage() {
   return (
     <div className="font-raleway">
       <Header />
-      <div className="flex flex-col items-center justify-center min-h-screen gap-16">
+      <div className="flex flex-col items-center justify-center min-h-screen gap-16 mb-12">
         <main className="flex flex-col gap-8 items-center justify-center">
           <HeroSection />
           <FeaturesSection />
         </main>
       </div>
+      <footer className="flex flex-col items-center justify-center gap-4 py-8 bg-neutral-900 text-gray-100">
+        <p>&copy; 2025 Synchrone. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
