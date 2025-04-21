36abac8e22a42591bc4afd4d9da89b40e8e79561
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-04-21 17:35:01 +0700

    feat: reduce size of title

--- synchrone/src/components/landing/FeatureCard.tsx
+++ synchrone/src/components/landing/FeatureCard.tsx
@@ -6,7 +6,7 @@ interface FeatureCardProps {
 export const FeatureCard = ({ title, description }: FeatureCardProps) => {
   return (
     <article className="size-full flex flex-col gap-2 p-4 bg-neutral-900 border-2 border-neutral-700 rounded-xl hover:bg-neutral-800 hover:border-blue-600 transition-colors duration-150">
-      <h3 className="text-2xl font-bold">{title}</h3>
+      <h3 className="text-xl font-bold">{title}</h3>
       <p className="">{description}</p>
     </article>
   );
